1937f2297ae3f1e28581d85a1f61e62b2a3ab1e6
Nandish2021 <ns.nandish2003@gmail.com>
2023-06-26 12:08:04 +0530

    add printTemplate

--- const.cpp
+++ const.cpp
@@ -1,8 +1,20 @@
-#include<iostream>
-using namespace std;
-int main() {
-    const string see;
-const string message = "Something went wrong please try again";
+#include <iostream>
+#include <string>
 
-return 0;
+using namespace std;
+
+template <typename T>
+void print(const T& value) {
+    cout << value << endl;
+}
+
+int main() {
+    const string message = "Something went wrong. Please try again."; // read-only string
+    const int a = 10;
+    const int* p = &a;
+
+    print(message);
+    print(to_string(a)); // Convert 'a' to string before passing it to the print function
+print(p);
+    return 0;
 }
