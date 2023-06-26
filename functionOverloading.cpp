b32fe5bfe55c97afcdc229afa53328458197a599
Nandish2021 <ns.nandish2003@gmail.com>
2023-06-26 12:34:58 +0530

    sum fun

--- functionOverloading.cpp
+++ functionOverloading.cpp
@@ -1,4 +1,24 @@
-//static polymorphism 
+//static polymorphism - decision made during compile time
+
+// #include<iostream>
+// using namespace std;
+// template <typename T>
+// void print(const T& value) {
+//     cout << value << endl;
+// }
+// double area(double side){
+//     return side*side;
+// }
+// double area(double width,double height){
+//     return width*height;
+// }
+
+// int main() {
+// print(area(2));
+// print(area(2,3));
+// return 0;
+// }
+
 
 #include<iostream>
 using namespace std;
@@ -6,15 +26,17 @@ template <typename T>
 void print(const T& value) {
     cout << value << endl;
 }
-double area(double side){
-    return side*side;
+int sum(int a,int b){
+   
+    return a+b;
 }
-double area(double width,double height){
-    return width*height;
+double sum(double a, double b){
+    return a+b;
 }
 
 int main() {
-print(area(2));
-print(area(2,3));
+ print(sum(1,2));
+ print(sum(1.4,1.2222));
+//  print(sum("s","d"));    -> no instance of overloaded function "sum" matches the argument list
 return 0;
 }
